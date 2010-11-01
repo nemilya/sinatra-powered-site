41f3b73aebce04ded76d8ddac10e70900ea5bd18
unknown <Ilya Nemihin@.(none)>
2010-11-01 14:26:34 +0500

    template powered

--- app.rb
+++ app.rb
@@ -1,6 +1,34 @@
 require 'rubygems'
 require 'sinatra'
 
+set :public, "./public"
+
+before do 
+  @site_layout = :'page/main'
+
+  @menu = []
+  @menu << ['/', 'Home']
+  @menu << ['/about', 'About']
+  @menu << ['/photos', 'Photos']
+  @menu << ['/contact', 'Contact']
+end
+
 get '/' do
-  "Hello from Sinatra on Heroku!"
+  @title = 'Pure Sinatra-rb example'
+  erb :index, :layout=> @site_layout
+end
+
+get '/about' do
+  @title = 'About'
+  erb :about, :layout=> @site_layout
+end
+
+get '/photos' do
+  @title = 'Photos'
+  erb :photos, :layout=> @site_layout
+end
+
+get '/contact' do
+  @title = 'Contact'
+  erb :contact, :layout=> @site_layout
 end
